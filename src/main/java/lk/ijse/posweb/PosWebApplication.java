fd6e6f8e2e93232f642af52b9c789c90cc305863
janindusathsara <janindusathsara1111@gmail.com>
2024-01-13 11:11:34 +0530

    first commit

---
+++ src/main/java/lk/ijse/posweb/PosWebApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.posweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/posweb/PosWebApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.posweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
